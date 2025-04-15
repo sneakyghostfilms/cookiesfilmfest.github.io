016d093b1001397aee4e9b19a7a809a540a79862
Scott Danzig <2577300+sdanzig@users.noreply.github.com>
2025-04-15 10:42:31 -0400

    Initial update of website for 2026

---
+++ src/data/festival2025.js
@@ -0,0 +1,404 @@
+// Import comedian images
+import derekMintoImg from '../assets/comedians/derek-minto.jpg';
+import amirMegheriImg from '../assets/comedians/amir-megheri.jpg';
+import kayleighDumasImg from '../assets/comedians/kayleigh-dumas.jpg';
+
+// Data structure for the 2025 Festival
+export const festival2025 = {
+  blocks: [
+    {
+      name: "The Derek Minto Block",
+      host: "Derek Minto",
+      hostImage: derekMintoImg.src,
+      time: "12:00PM – 2:20PM",
+      kickoff: "Comedy Kickoff with Derek Minto",
+      intermission: "Intermission with Derek Minto",
+      intermissionAfterFilm: 5,
+      qa: "Filmmaker Q&A session",
+      venue: "Row House Cinema",
+      films: [
+        {
+          title: "Convicts & Chocolate",
+          director: "Brandie Lee McDonald",
+          runtime: "11min",
+          synopsis: "In this action-packed, high octane hero swap, comedy spoof with Forrest Gump trying to save the day in the 1997 film, Con Air.",
+          country: "United States"
+        },
+        {
+          title: "Lil' Somethin' in: Clash with the Cube Demon",
+          director: "Dan Sollaccio",
+          runtime: "3:25",
+          synopsis: "Lil' Somethin', a cartoony blob, is targeted by a planet-sized, cube-shaped demon and must find a way to survive—using some rather unusual problem-solving skills.",
+          country: "United States"
+        },
+        {
+          title: "Slasher Sessions",
+          director: "Thomas A Fowler",
+          runtime: "20min",
+          synopsis: "Five notorious mass murderers go to group therapy.",
+          country: "United States"
+        },
+        {
+          title: "Chocolate Chip",
+          director: "Jim Hails",
+          runtime: "12:15",
+          synopsis: "A pregnant woman has a sudden craving for cookies in the middle of the night.",
+          country: "United States"
+        },
+        {
+          title: "GNOME$",
+          director: "Matthew Diulus",
+          runtime: "3:35",
+          synopsis: "A man must choose between his hunger and gnome friends.",
+          country: "United States"
+        },
+        {
+          title: "Pizza Man",
+          director: "Nathan Miller",
+          runtime: "4:20",
+          synopsis: "A pizza man accidentally delivers a pizza to a super villain's newly constructed fortress.",
+          country: "United States",
+          award: "Best Local Short"
+        },
+        {
+          title: "Haircut",
+          director: "Jase Klatzkin, Marshall McCall",
+          runtime: "3:03",
+          synopsis: "After a disastrous trip to the barber, Marshall has a new haircut that cannot be ignored.",
+          country: "United States"
+        },
+        {
+          title: "Goodbye Beard",
+          director: "Ryan Iverson",
+          runtime: "6:27",
+          synopsis: "A man goes on the run after being mistaken for Santa by a group of ruthless children, and meets the real Santa, who has his own sinister agenda.",
+          country: "United States",
+          award: "Honorable Mention"
+        },
+        {
+          title: "Ted Gold Did It",
+          director: "Lucas Lara Esteves",
+          runtime: "7:25",
+          synopsis: "One Dead Body. Four Suspects. One Eaten Tray of Tiramisu.",
+          country: "United States"
+        },
+        {
+          title: "Rysy",
+          director: "Tuomo Mäntynen",
+          runtime: "4:02",
+          synopsis: "A beautiful summer day, an innocent car ride and an evil doll. Not the average day one might expect.",
+          country: "Finland"
+        },
+        {
+          title: "Latte For Jim",
+          director: "JJ Zink",
+          runtime: "7:07",
+          synopsis: "When two men named Jim mistake their coffee orders for one another's, they will stop at nothing to get their daily dose of crucial caffeine.",
+          country: "United States"
+        },
+        {
+          title: "Dr. Taker's Every-Ailment Strips",
+          director: "Eli Crowther",
+          runtime: "10:37",
+          synopsis: "When an inventor invites his best college friends into town for a weekend, things start to escalate into hilarious madness as they find out the wonders of Dr Taker's Every Ailment Strips.",
+          country: "United States"
+        },
+        {
+          title: "The Allergy Table",
+          director: "Pat Battistini",
+          runtime: "6:20",
+          synopsis: "When the new kid shows up, the regulars show him the rules and duties of eating at a grade school allergy table.",
+          country: "United States"
+        }
+      ]
+    },
+    {
+      name: "The Amir Megheri Block",
+      host: "Amir Megheri",
+      hostImage: amirMegheriImg.src,
+      time: "2:40PM – 5:00PM",
+      kickoff: "Comedy Kickoff with Amir Megheri",
+      intermission: "Intermission with Amir Megheri",
+      intermissionAfterFilm: 6,
+      qa: "Filmmaker Q&A session",
+      venue: "Row House Cinema",
+      films: [
+        {
+          title: "Senior Software Engineer",
+          director: "Scott Danzig",
+          runtime: "15:00",
+          synopsis: "A grandma programmer allies with the ghost of her predecessor to satisfy her young, trendy influencer boss's latest ultimatum.",
+          country: "United States"
+        },
+        {
+          title: "Bernie",
+          director: "Caroline Collins",
+          runtime: "15:51",
+          synopsis: "A half-hearted suicide attempt reunites two polar-opposite brothers in this dark comedy.",
+          country: "United States",
+          award: "Second to One Local Short, Best Local Comedic Acting"
+        },
+        {
+          title: "Glazed Over",
+          director: "Lucas Lara Esteves",
+          runtime: "8:13",
+          synopsis: "Two internet personalities bite off too much when they investigate a weird baking cult.",
+          country: "United States",
+          award: "Second to One Local Comedic Acting"
+        },
+        {
+          title: "Lucky Saturday",
+          director: "Leo W Mahoney",
+          runtime: "1:03",
+          synopsis: "A normal Saturday turns into a great Saturday.",
+          country: "United States"
+        },
+        {
+          title: "SUPERCRITICAL",
+          director: "John Osment",
+          runtime: "7:18",
+          synopsis: "In a post-apocalyptic nuclear fallout shelter, a young scientist bothers their team leader with a series of progressively inane HR requests.",
+          country: "United States",
+          award: "Honorable Mention"
+        },
+        {
+          title: "This is not Norway",
+          director: "Alicia Albares, Paco Cavero",
+          runtime: "17:59",
+          synopsis: "Jorge is dead-set on honoring his father's wish for a Viking funeral at sea—no matter the obstacles.",
+          country: "Spain",
+          award: "Best Foreign Film"
+        },
+        {
+          title: "Dr. Pepper Movie Trailer",
+          director: "Katherine Coleman",
+          runtime: "2:18",
+          synopsis: "A movie trailer parody of a biopic about the creator of Dr. Pepper.",
+          country: "United States"
+        },
+        {
+          title: "Wheel",
+          director: "Honora Talbott",
+          runtime: "9:15",
+          synopsis: "A casting director gets hit with some bad news while helping her eccentric neighbor tape her Wheel of Fortune audition.",
+          country: "United States",
+          award: "Second to One Short Film, Best Comedic Acting"
+        },
+        {
+          title: "The Trouble With Cake...And Eating It Too",
+          director: "Darin DiNapoli, Brad Grimm",
+          runtime: "7:41",
+          synopsis: "A piece of cake goes missing at a TV studio which results in one co-worker questioning the entire staff.",
+          country: "United States"
+        },
+        {
+          title: "The Ten Commandments",
+          director: "Tyler J. Oakley",
+          runtime: "4:55",
+          synopsis: "On new spin on the old tale. As the town of Sinai faces chaos and disorder, Moses comes down from the mountain to lay down a few rules.",
+          country: "United States"
+        },
+        {
+          title: "The Double Life of Hugh",
+          director: "Benjamin Z Tobin",
+          runtime: "8:57",
+          synopsis: "Hugh and his father Hugo have very different ideas about what it means to be a mime.",
+          country: "United States"
+        }
+      ]
+    },
+    {
+      name: "The Kayleigh Dumas Block",
+      host: "Kayleigh Dumas",
+      hostImage: kayleighDumasImg.src,
+      time: "5:40PM – 8:00PM",
+      kickoff: "Comedy Kickoff with Kayleigh Dumas",
+      intermission: "Intermission with Kayleigh Dumas",
+      intermissionAfterFilm: 6,
+      qa: "Filmmaker Q&A session",
+      venue: "Row House Cinema",
+      films: [
+        {
+          title: "Badmouth Grandma",
+          director: "Timothy Blackwood",
+          runtime: "5:38",
+          synopsis: "Down-and-out comic John lands a last-minute headlining gig at LA's biggest club—he's got just four hours to craft new material, and his no-nonsense Siberian grandmother isn't letting him off easy.",
+          country: "United States"
+        },
+        {
+          title: "Cowboy Love Songs",
+          director: "Tyler J. Oakley",
+          runtime: "4:59",
+          synopsis: "As a band of heartbroken cowboys sit in the saloon to singing tale of bygone love, a newcomer enters to add verse of his own...but misses the point.",
+          country: "United States"
+        },
+        {
+          title: "Turkey Lover",
+          director: "Ted Omo",
+          runtime: "9:00",
+          synopsis: "When a fringe friend with a history of devious food-based behavior shows up to Friendsgiving, the rest of the guests try to diffuse the situation before the entire day goes horribly wrong.",
+          country: "United States",
+          award: "Second to One Comedic Acting"
+        },
+        {
+          title: "Silent, But Deadly",
+          director: "Jack Colin",
+          runtime: "6:00",
+          synopsis: "A comedic short film about a first date and an unclaimed fart.",
+          country: "United States"
+        },
+        {
+          title: "With Tits",
+          director: "Delia Morchesky",
+          runtime: "3:00",
+          synopsis: "A kick-ass high school basketball team takes a stand against their sexist principal to get the name they deserve.",
+          country: "United States",
+          award: "Honorable Mention"
+        },
+        {
+          title: "ILY, BYE",
+          director: "Taylor James",
+          runtime: "13:28",
+          synopsis: "Unemployed and down on her luck, Siobhan is presented with a life-changing job opportunity. Unfortunately, she can't get her foot out of her mouth long enough to get her foot in the door.",
+          country: "United States",
+          award: "Best Short Film"
+        },
+        {
+          title: "To Pee or Not To Pee",
+          director: "Jamen A Meistrich",
+          runtime: "8:43",
+          synopsis: "A young man on a road trip runs into several obstacles in his search for a bathroom, which leaves his with a difficult choice.",
+          country: "United States",
+          award: "Honorable Mention"
+        },
+        {
+          title: "Beethoven's Great Great Great Great Great Grandchild",
+          director: "Daniel Maggio",
+          runtime: "12:30",
+          synopsis: "The last living descendent of Beethoven tries to capitalize on their heritage, but they have no musical talent whatsoever.",
+          country: "United States",
+          award: "Honorable Mention"
+        },
+        {
+          title: "The One",
+          director: "Molly Parsons, Ellery Stocker",
+          runtime: "3:21",
+          synopsis: "Sexy med student Carter as he competes against MDW, the Mongolian Death Worm, for the hand of Kenzie, the bachelorette.",
+          country: "Not Specified",
+          award: "Best Super Short"
+        },
+        {
+          title: "Ms. Rossi 2: Ms. Rossi Takes The Cake",
+          director: "Pat Battistini",
+          runtime: "17:58",
+          synopsis: "While on her travels as a Loan Officer for the bank, Ms. Rossi encounters some obstacles along the way while just trying to do her job.",
+          country: "Italy",
+          award: "Second to One Foreign Film"
+        },
+        {
+          title: "Mama Rose's Pound Cake",
+          director: "Mark Edward Sarro",
+          runtime: "11:07",
+          synopsis: "Burt and Layme (Whaling Soles) are off in search of the mysterious land of Cakewich where the Cult of Bakers are having a secret ritual to conjure Mama Rose's Pound Cake.",
+          country: "United States"
+        },
+        {
+          title: "Gynie on the Go",
+          director: "Roya Hamadani, Jiah Peck",
+          runtime: "2:29",
+          synopsis: "Satirical commercial for a drive through gynecologist.",
+          country: "United States"
+        },
+        {
+          title: "Brush Me",
+          director: "Nikolaus Jantsch",
+          runtime: "2:00",
+          synopsis: "In a bustling bathroom, amidst the chaos of everyday life, two toothbrushes, find themselves drawn to each other.",
+          country: "Austria"
+        }
+      ]
+    }
+  ],
+  awardsInfo: {
+    ceremony: "8:00PM",
+    description: "Join us as we honor this year's standouts and enjoy our famous cookie table!",
+    venue: "Row House Cinema"
+  },
+  venueInfo: {
+    name: "Row House Cinema",
+    description: "A single-screen theater in Pittsburgh's Lawrenceville neighborhood, showing classic, foreign, and cult films.",
+    address: "4115 Butler St, Pittsburgh, PA 15201"
+  }
+};
+
+// Group award winners by category
+export const awardCategories2025 = [
+  {
+    categoryName: "Short Film",
+    awards: [
+      { title: "Best", winner: "ILY, BYE", creator: "Taylor James" },
+      { title: "Second to One", winner: "The Wheel", creator: "Honora Talbott" }
+    ]
+  },
+  {
+    categoryName: "Super Short",
+    awards: [
+      { title: "Best", winner: "The One", creator: "Molly Parsons and Ellery Stocker" },
+      { title: "Second to One", winner: "Pizza Man", creator: "Nathan Miller" }
+    ]
+  },
+  {
+    categoryName: "Foreign Film",
+    awards: [
+      { title: "Best", winner: "This is Not Norway", creator: "Alicia Albares and Paco Cavero" },
+      { title: "Second to One", winner: "Ms Rossi 2: Ms. Rossi Takes the Cake", creator: "Pat Battistini" }
+    ]
+  },
+  {
+    categoryName: "Comedic Acting",
+    awards: [
+      { title: "Best", winner: "The Wheel", creator: "Honora Talbott" },
+      { title: "Second to One", winner: "Turkey Lover", creator: "Ted Omo" }
+    ]
+  },
+  {
+    categoryName: "Local Short",
+    awards: [
+      { title: "Best", winner: "Pizza Man", creator: "Nathan Miller" },
+      { title: "Second to One", winner: "Bernie", creator: "Caroline Collins" }
+    ]
+  },
+  {
+    categoryName: "Local Comedic Acting",
+    awards: [
+      { title: "Best", winner: "Bernie", creator: "Caroline Collins" },
+      { title: "Second to One", winner: "Glazed Over", creator: "Lucas Lara Esteves" }
+    ]
+  },
+  {
+    categoryName: "Honorable Mentions",
+    awards: [
+      { title: "", winner: "Beethoven's Great Great Great Great Great Grandchild", creator: "Daniel Maggio" },
+      { title: "", winner: "Goodbye Beard", creator: "Ryan Iverson" },
+      { title: "", winner: "SUPERCRITICAL", creator: "John Osment" },
+      { title: "", winner: "To Pee or Not To Pee", creator: "Jamen A Meistrich" },
+      { title: "", winner: "With Tits", creator: "Delia Morchesky" }
+    ]
+  }
+];
+
+// Keep the original array format for backward compatibility
+export const awardWinners2025 = [
+  { category: "Best Short Film", winner: "ILY, BYE", creator: "Taylor James" },
+  { category: "Second to One Short Film", winner: "The Wheel", creator: "Honora Talbott" },
+  { category: "Best Super Short", winner: "The One", creator: "Molly Parsons and Ellery Stocker" },
+  { category: "Second to One Super Short", winner: "Pizza Man", creator: "Nathan Miller" },
+  { category: "Best Foreign Film", winner: "This is Not Norway", creator: "Alicia Albares and Paco Cavero" },
+  { category: "Second to One Foreign Film", winner: "Ms Rossi 2: Ms. Rossi Takes the Cake", creator: "Pat Battistini" },
+  { category: "Best Comedic Acting", winner: "The Wheel", creator: "Honora Talbott" },
+  { category: "Second to One Comedic Acting", winner: "Turkey Lover", creator: "Ted Omo" },
+  { category: "Best Local Short", winner: "Pizza Man", creator: "Nathan Miller" },
+  { category: "Second to One Local Short", winner: "Bernie", creator: "Caroline Collins" },
+  { category: "Best Local Comedic Acting", winner: "Bernie", creator: "Caroline Collins" },
+  { category: "Second to One Local Comedic Acting", winner: "Glazed Over", creator: "Lucas Lara Esteves" }
+]; 
